d4b4bda97835e3d8a6de806e1f088cf8652b0ec7
Thrill <105758293+Thrill32@users.noreply.github.com>
2026-03-05 09:57:13 -0500

    Initial Commit

---
+++ Stunskin2/Stunskin2App.swift
@@ -0,0 +1,17 @@
+//
+//  Stunskin2App.swift
+//  Stunskin2
+//
+//  Created by Will Morreale on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Stunskin2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
